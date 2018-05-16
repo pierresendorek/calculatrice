2063d33efa3c0c77bf4bc981043625e0f3b2ab01
Pierre SENDOREK <psendorek@xebia.fr>
2018-05-16 14:18:32 +0200

    Clean done

--- main.py
+++ main.py
@@ -3,23 +3,13 @@ from addition import add
 
 
 sys_argv = sys.argv[1:]
-print(sys_argv)
-
 op, a, b = sys_argv
-
 a, b = int(a), int(b)
-
 op_function = lambda a, b: a
-
-
 
 if op == 'add':
     print("affect add")
     op_function = add
-
-
-print(op)
-print(type(op))
 
 
 print("result = " , op_function(a,b))
